2ff0fed23d5a7233cf6842f6139f11affe90643c
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-12 07:22:59 +0900

    feat(ruby): add test for Analyzer.new

--- bindings/ruby/test/test_ebur128_stream.rb
+++ bindings/ruby/test/test_ebur128_stream.rb
@@ -9,7 +9,21 @@ class EBUR128StreamTest < Test::Unit::TestCase
     end
   end
 
-  test "something useful" do
-    assert_equal("expected", "actual")
+  test "new" do
+    assert_raise ArgumentError do
+      EBUR128Stream::Analyzer.new
+    end
+    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:left])
+    assert_raise ArgumentError do
+      EBUR128Stream::Analyzer.new(channels: [:left, :nothing])
+    end
+    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:center], sample_rate: 48_000)
+    assert_raise RuntimeError do
+      EBUR128Stream::Analyzer.new(channels: [:center], modes: [])
+    end
+    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:center], modes: [:integrated, :true_peak])
+    assert_raise ArgumentError do
+      EBUR128Stream::Analyzer.new(channels: [:center], modes: [:unknown])
+    end
   end
 end
