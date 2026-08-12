c2329bc950b4cd0d68fefd3243c3ce48cc38597e
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-12 18:21:38 +0900

    feat(ruby): define Analyzer#push_planar

--- bindings/ruby/ext/ebur128_stream/src/lib.rs
+++ bindings/ruby/ext/ebur128_stream/src/lib.rs
@@ -1,5 +1,7 @@
 use core::time::Duration;
-use ebur128_stream_rs::{Analyzer as AnalyzerRs, AnalyzerBuilder, Channel, Mode, Report as RsReport};
+use ebur128_stream_rs::{
+    Analyzer as AnalyzerRs, AnalyzerBuilder, Channel, Error as RsError, Mode, Report as RsReport,
+};
 use magnus::{
     Error, Integer, RArray, Ruby, Symbol, TryConvert, Value, function, method,
     prelude::*,
@@ -117,7 +119,50 @@ impl Analyzer {
         Ok(())
     }
 
-    fn push_planner() {}
+    fn push_planar(ruby: &Ruby, rb_self: &Self, samples: Value) -> Result<(), Error> {
+        let samples: Vec<Vec<f32>> = if let Some(channels) = RArray::from_value(samples) {
+            channels
+                .into_iter()
+                .map(|channel| {
+                    let ch = RArray::from_value(channel).ok_or_else(|| {
+                        Error::new(
+                            ruby.exception_arg_error(),
+                            format!("channel not Array: {channel:?}"),
+                        )
+                    })?;
+                    ch.into_iter()
+                        .map(f32::try_convert)
+                        .collect::<Result<Vec<f32>, Error>>()
+                })
+                .collect::<Result<Vec<Vec<f32>>, Error>>()?
+        } else {
+            return Err(Error::new(
+                ruby.exception_arg_error(),
+                format!("unsupported samples type: {samples:?}"),
+            ));
+        };
+        let samples: Vec<&[f32]> = samples.iter().map(|channel| &channel[..]).collect();
+
+        let mut analyzer = rb_self
+            .analyzer
+            .try_borrow_mut()
+            .map_err(|_| Error::new(ruby.exception_runtime_error(), "analyzer already in use"))?;
+        let analyzer = analyzer.as_mut().ok_or_else(|| {
+            Error::new(ruby.exception_runtime_error(), "analyzer not initialized")
+        })?;
+        analyzer.push_planar(&samples).map_err(|err| match err {
+            RsError::ChannelMismatch { expected: _, got: _ } => {
+                Error::new(ruby.exception_arg_error(), format!("{err:?}"))
+            }
+            RsError::PlanarLengthMismatch { first: _, got: _ } => {
+                Error::new(ruby.exception_arg_error(), format!("{err:?}"))
+            }
+            RsError::NonFiniteSample => Error::new(ruby.exception_arg_error(), format!("{err:?}")),
+            _ => unreachable!(),
+        })?;
+
+        Ok(())
+    }
 
     fn finalize(ruby: &Ruby, rb_self: &Self) -> Result<Report, Error> {
         let mut analyzer = rb_self
@@ -144,6 +189,7 @@ fn init(ruby: &Ruby) -> Result<(), Error> {
     let analyzer = ebur128_stream.define_class("Analyzer", ruby.class_object())?;
     analyzer.define_singleton_method("new", function!(Analyzer::new, -1))?;
     analyzer.define_method("push_interleaved", method!(Analyzer::push_interleaved, 1))?;
+    analyzer.define_method("push_planar", method!(Analyzer::push_planar, 1))?;
     analyzer.define_method("finalize", method!(Analyzer::finalize, 0))?;
     let report = ebur128_stream.define_class("Report", ruby.class_object())?;
 
