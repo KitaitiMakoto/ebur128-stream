3ec129bb12ee7b9967af4599a371256d7d42c1bb
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-13 04:12:57 +0900

    feat(ruby): add test for Normalizer#normalize_in_place

--- bindings/ruby/test/test_normalizer.rb
+++ bindings/ruby/test/test_normalizer.rb
@@ -6,4 +6,19 @@ class TestNormalizer < Test::Unit::TestCase
   def test_new
     assert_instance_of Normalizer, Normalizer.new(sample_rate: 48_000, channels: [:left, :right])
   end
+
+  def test_normalize_in_place
+    sample_rate = 48_000
+    normalizer = Normalizer.new(channels: [:left, :right], sample_rate:)
+    frame = 2 * Math::PI / (sample_rate / 4)
+    samples = sample_rate.times.flat_map do |n|
+      value = Math.cos(frame * n) * 10
+    end
+    before = samples.dup
+    normalizer.normalize_in_place(samples)
+
+    assert do
+      samples != before
+    end
+  end
 end
