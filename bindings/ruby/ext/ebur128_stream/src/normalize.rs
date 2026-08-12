15f9985c56c6788f7906c4d04c76ac4932c9d1d7
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-13 02:03:29 +0900

    feat(ruby): add NormalizeReport

--- bindings/ruby/ext/ebur128_stream/src/normalize.rs
+++ bindings/ruby/ext/ebur128_stream/src/normalize.rs
@@ -1,7 +1,7 @@
 use crate::parse_channels_arg;
 use ebur128_stream_rs as engine;
 use magnus::{
-    Error, RArray, RModule, Ruby, Value, function,
+    Error, RArray, RModule, Ruby, Value, function, method,
     prelude::*,
     scan_args::{get_kwargs, scan_args},
 };
@@ -37,9 +37,63 @@ impl Normalizer {
     }
 }
 
+#[magnus::wrap(class = "EBUR128Stream::NormalizeReport")]
+struct NormalizeReport {
+    report: engine::normalize::NormalizeReport,
+}
+
+impl NormalizeReport {
+    fn measured_integrated_lufs(rb_self: &Self) -> Option<f64> {
+        rb_self.report.measured_integrated_lufs
+    }
+
+    fn measured_true_peak_dbtp(rb_self: &Self) -> Option<f64> {
+        rb_self.report.measured_true_peak_dbtp
+    }
+
+    fn target_lufs(rb_self: &Self) -> f64 {
+        rb_self.report.target_lufs
+    }
+
+    fn true_peak_ceiling_dbtp(rb_self: &Self) -> Option<f64> {
+        rb_self.report.true_peak_ceiling_dbtp
+    }
+
+    fn applied_gain_db(rb_self: &Self) -> f64 {
+        rb_self.report.applied_gain_db
+    }
+
+    fn limited_by_true_peak(rb_self: &Self) -> bool {
+        rb_self.report.limited_by_true_peak
+    }
+}
+
 pub(crate) fn init(ruby: &Ruby, module: &RModule) -> Result<(), Error> {
     let normalizer = module.define_class("Normalizer", ruby.class_object())?;
     normalizer.define_singleton_method("new", function!(Normalizer::new, -1))?;
+
+    let normalize_report = module.define_class("NormalizeReport", ruby.class_object())?;
+    normalize_report.define_method(
+        "measured_integrated_lufs",
+        method!(NormalizeReport::measured_integrated_lufs, 0),
+    )?;
+    normalize_report.define_method(
+        "measured_true_peak_dbtp",
+        method!(NormalizeReport::measured_true_peak_dbtp, 0),
+    )?;
+    normalize_report.define_method("target_lufs", method!(NormalizeReport::target_lufs, 0))?;
+    normalize_report.define_method(
+        "true_peak_ceiling_dbtp",
+        method!(NormalizeReport::true_peak_ceiling_dbtp, 0),
+    )?;
+    normalize_report.define_method(
+        "applied_gain_db",
+        method!(NormalizeReport::applied_gain_db, 0),
+    )?;
+    normalize_report.define_method(
+        "limited_by_true_peak",
+        method!(NormalizeReport::limited_by_true_peak, 0),
+    )?;
 
     Ok(())
 }
