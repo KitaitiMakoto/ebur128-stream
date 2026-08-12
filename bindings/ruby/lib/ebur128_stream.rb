e5eba5ef6c5b5034c16b1e267a8db707027c0ead
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-12 20:30:36 +0900

    feat(ruby): customize Report#inspect

--- bindings/ruby/lib/ebur128_stream.rb
+++ bindings/ruby/lib/ebur128_stream.rb
@@ -5,5 +5,22 @@ require "ebur128_stream/ebur128_stream"
 
 module EBUR128Stream
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Report
+    ATTRS = [
+      :integrated_lufs,
+      :loudness_range_lu,
+      :true_peak_dbtp,
+      :momentary_max_lufs,
+      :short_term_max_lufs,
+      :programme_duration_seconds
+    ]
+
+    def inspect
+      "#<%{class} %{attrs}>" % {
+        class: self.class,
+        attrs: ATTRS.collect {|attr| "#{attr}=#{send(attr) || nil.inspect}"}.join(" ")
+      }
+    end
+  end
 end
