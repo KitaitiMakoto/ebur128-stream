019925cb2b6eae1487549bf3f4f3dfb639b131d5
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-15 07:13:19 +0900

    chore(ruby): Remove module qualification

--- bindings/ruby/test/test_ebur128_stream.rb
+++ bindings/ruby/test/test_ebur128_stream.rb
@@ -3,6 +3,8 @@
 require_relative "helper"
 
 class EBUR128StreamTest < Test::Unit::TestCase
+  include EBUR128Stream
+
   test "VERSION" do
     assert do
       ::EBUR128Stream.const_defined?(:VERSION)
@@ -11,24 +13,24 @@ class EBUR128StreamTest < Test::Unit::TestCase
 
   test "new" do
     assert_raise ArgumentError do
-      EBUR128Stream::Analyzer.new
+      Analyzer.new
     end
-    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:left])
+    assert_instance_of Analyzer, Analyzer.new(channels: [:left])
     assert_raise ArgumentError do
-      EBUR128Stream::Analyzer.new(channels: [:left, :nothing])
+      Analyzer.new(channels: [:left, :nothing])
     end
-    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:center], sample_rate: 48_000)
+    assert_instance_of Analyzer, Analyzer.new(channels: [:center], sample_rate: 48_000)
     assert_raise RuntimeError do
-      EBUR128Stream::Analyzer.new(channels: [:center], modes: [])
+      Analyzer.new(channels: [:center], modes: [])
     end
-    assert_instance_of EBUR128Stream::Analyzer, EBUR128Stream::Analyzer.new(channels: [:center], modes: [:integrated, :true_peak])
+    assert_instance_of Analyzer, Analyzer.new(channels: [:center], modes: [:integrated, :true_peak])
     assert_raise ArgumentError do
-      EBUR128Stream::Analyzer.new(channels: [:center], modes: [:unknown])
+      Analyzer.new(channels: [:center], modes: [:unknown])
     end
   end
 
   test "push_interleaved" do
-    analyzer = EBUR128Stream::Analyzer.new(channels: [:left, :right], modes: [:all])
+    analyzer = Analyzer.new(channels: [:left, :right], modes: [:all])
     assert_nothing_raised do
       analyzer.push_interleaved [1.0, 1.0, 2.0, 2.0, 3.0, 3.0]
     end
@@ -38,7 +40,7 @@ class EBUR128StreamTest < Test::Unit::TestCase
   end
 
   test "push_planar" do
-    analyzer = EBUR128Stream::Analyzer.new(channels: [:left, :right], modes: [:all])
+    analyzer = Analyzer.new(channels: [:left, :right], modes: [:all])
     assert_nothing_raised do
       analyzer.push_planar [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]
       analyzer.push_planar [[1.0, 2.0, 3.0], [1.0, 2.0, 3.0]]
@@ -49,13 +51,13 @@ class EBUR128StreamTest < Test::Unit::TestCase
   end
 
   test "finalize" do
-    analyzer = EBUR128Stream::Analyzer.new(channels: [:left, :right], modes: [:all])
+    analyzer = Analyzer.new(channels: [:left, :right], modes: [:all])
     analyzer.push_interleaved [1.0, 1.0, 2.0, 2.0, 3.0, 3.0]
     report = nil
     assert_nothing_raised do
       report = analyzer.finalize
     end
-    assert_instance_of EBUR128Stream::Report, report
+    assert_instance_of Report, report
     assert_raise_with_message RuntimeError, /finalized/ do
       analyzer.finalize
     end
