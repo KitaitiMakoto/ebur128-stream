eb1b71a8bef85f1db3f55a7ef5d53d3d55d06513
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-16 02:01:56 +0900

    feat(ruby): add Channels

--- bindings/ruby/ext/ebur128_stream/src/lib.rs
+++ bindings/ruby/ext/ebur128_stream/src/lib.rs
@@ -13,7 +13,7 @@ use crate::{
     snapshot::Snapshot,
 };
 use ebur128_stream_rs as engine;
-use magnus::{RArray, Ruby, Symbol, TryConvert};
+use std::ops::Deref;
 
 // can make channels a slice?
 pub(crate) fn parse_channels_arg(channels: RArray) -> Result<Vec<engine::Channel>, Error> {
@@ -35,6 +35,55 @@ pub(crate) fn parse_channels_arg(channels: RArray) -> Result<Vec<engine::Channel
             }
         })
         .collect()
+}
+
+pub(crate) struct Channels {
+    inner: Vec<engine::Channel>,
+}
+
+impl Deref for Channels {
+    type Target = Vec<engine::Channel>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl TryConvert for Channels {
+    fn try_convert(val: Value) -> Result<Self, magnus::Error> {
+        use engine::Channel::*;
+
+        let array = RArray::try_convert(val)?;
+        let result: Result<Vec<engine::Channel>, magnus::Error> = array
+            .into_iter()
+            .map(|value| {
+                let ch = Symbol::try_convert(value)?;
+                match ch.name()?.as_ref() {
+                    "left" => Ok(Left),
+                    "right" => Ok(Right),
+                    "center" => Ok(Center),
+                    "left_surround" => Ok(LeftSurround),
+                    "right_surround" => Ok(RightSurround),
+                    "lfe" => Ok(Lfe),
+                    "other" => Ok(Other),
+                    _ => {
+                        let ruby = Ruby::get_with(val);
+                        Err(magnus::Error::new(
+                            ruby.exception_runtime_error(),
+                            format!("unknown channel: {ch}"),
+                        ))
+                    }
+                }
+            })
+            .collect();
+        Ok(Self { inner: result? })
+    }
+}
+
+impl Channels {
+    fn into_boxed_slice(self) -> Box<[engine::Channel]> {
+        self.inner.into_boxed_slice()
+    }
 }
 
 #[magnus::init]
