96cfb4d42c1af4cc7905cfea2efe6092fcf696d1
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-08-15 09:15:20 +0900

    feat(ruby): wrap rb_memory_view_release by protect

--- bindings/ruby/ext/ebur128_stream/src/memory_view.rs
+++ bindings/ruby/ext/ebur128_stream/src/memory_view.rs
@@ -116,10 +116,13 @@ pub struct MemoryView<T> {
 
 impl<T> Drop for MemoryView<T> {
     fn drop(&mut self) {
-        // SAFETY: This guard is generated from successfully allocated rb_memory_view_t and drop() is called only once so it's not released more than once
-        unsafe {
-            rb_memory_view_release(&mut self.inner);
-        }
+        let _ = magnus::rb_sys::protect(|| {
+            // SAFETY: This guard is generated from successfully allocated rb_memory_view_t and drop() is called only once so it's not released more than once
+            unsafe {
+                rb_memory_view_release(&mut self.inner);
+            }
+            Qnil.into()
+        });
     }
 }
 
